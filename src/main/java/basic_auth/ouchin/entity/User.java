1032725cfb05d065d18f52c08dcac9345582346d
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-29 21:10:32 +0100

    create folder structure & User entity and repositorey

---
+++ src/main/java/basic_auth/ouchin/entity/User.java
@@ -0,0 +1,27 @@
+package basic_auth.ouchin.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+
+    private boolean enabled = true;
+}
---
+++ src/test/java/basic_auth/ouchin/BasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package basic_auth.ouchin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
